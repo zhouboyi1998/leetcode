a7aee1d02b7d18af9fe71148beb484b00f943504
zhouboyi <1144188685@qq.com>
2022-08-17 15:05:01 +0800

    feat(markdown): 显示Markdown文件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <markdown class="markdown-body"></markdown>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// 查询 ../../public/markdown 文件夹, true 同时查询子目录, /\.md$/ 正则表达式匹配 markdown 文件
+const requireComponent = require.context('../../public/markdown', true, /\.md$/)
+
+// 存储所有获取到的 markdown 文件
+let markdownList = {}
+
+requireComponent.keys().forEach(fileName => {
+    // 获取文件名
+    let name = fileName.split('/').pop().replace(/\.\w+$/, '')
+    // 获取组件配置
+    const config = requireComponent(fileName)
+    // 若该组件是通过 export default 导出的, 优先使用 .default, 否则使用模块的根
+    markdownList[name] = config.default || config
+})
+
+// 获取当前显示的 markdown 文件
+let Markdown = markdownList['one']
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'HomeView',
+    components: {
+        Markdown
+    }
 }
 </script>
