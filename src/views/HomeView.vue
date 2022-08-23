1fb90ce321a71f160bb683307f45a5e7df03ed59
zhouboyi <1144188685@qq.com>
2022-08-23 15:13:47 +0800

    fix(markdown): 更换过时的Markdown语法

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,15 +8,32 @@
                     active-text-color="#00AF9B"
                 >
                     <el-menu-item
-                        v-for="name in nameList" :key="name" :index="name"
+                        v-for="name in allList" :key="name" :index="name"
                         @click="chooseMarkdown(name)"
                     >
                         <p class="item">{{ name }}</p>
                     </el-menu-item>
                 </el-menu>
-                <el-tag class="ml-2 tag" type="success" effect="dark" color="#00AF9B">
-                    Note Total Number {{ noteNumber }}
-                </el-tag>
+                <div>
+                    <el-tag class="ml-2 tag" type="success" effect="dark" color="#00AF9B" @click="clickTag($event)">
+                        Easy Note Number {{ easyNumber }}
+                    </el-tag>
+                </div>
+                <div>
+                    <el-tag class="ml-2 tag" type="warning" effect="dark" color="#FFB822" @click="clickTag($event)">
+                        Medium Note Number {{ mediumNumber }}
+                    </el-tag>
+                </div>
+                <div>
+                    <el-tag class="ml-2 tag" type="danger" effect="dark" color="#FF2D55" @click="clickTag($event)">
+                        Hard Note Number {{ hardNumber }}
+                    </el-tag>
+                </div>
+                <div>
+                    <el-tag class="ml-2 tag" type="" effect="dark" color="#AF52DE" @click="clickTag($event)">
+                        All Note Number {{ allNumber }}
+                    </el-tag>
+                </div>
             </el-scrollbar>
         </el-aside>
         <el-main>
@@ -29,33 +46,59 @@
 
 <script setup>
 import { ref } from 'vue'
-
-// 查询 /markdown 文件夹中的 Markdown 文件
-// true 同时查询子目录, 正则表达式匹配 .md 文件
-const requireComponent = require.context('../../markdown', true, /\.md$/)
+import { ElNotification } from 'element-plus'
 
 // 存储所有获取到的 Markdown 文件
 let markdownList = {}
 
-// Markdown 文件名列表
-let nameList = []
+// 配置 Markdown 文件导出为组件
+const configureComponent = (component, list) => {
+    component.keys().forEach(fileName => {
+        // 获取文件名
+        let name = fileName.split('/').pop().replace(/\.\w+$/, '')
+        // 存储文件名
+        list.push(name)
+        // 获取组件配置
+        const config = component(fileName)
+        // 若该组件是通过 export default 导出的, 优先使用 .default, 否则使用模块的根
+        markdownList[name] = config.default || config
+    })
+}
 
-requireComponent.keys().forEach(fileName => {
-    // 获取文件名
-    let name = fileName.split('/').pop().replace(/\.\w+$/, '')
-    // 存储文件名
-    nameList.push(name)
-    // 获取组件配置
-    const config = requireComponent(fileName)
-    // 若该组件是通过 export default 导出的, 优先使用 .default, 否则使用模块的根
-    markdownList[name] = config.default || config
-})
+// 查询文件夹中的 Markdown 文件, true 同时查询子目录, 正则表达式匹配 .md 文件
+const helloComponent = require.context('../../markdown/hello', true, /\.md$/)
+const easyComponent = require.context('../../markdown/easy', true, /\.md$/)
+const mediumComponent = require.context('../../markdown/medium', true, /\.md$/)
+const hardComponent = require.context('../../markdown/hard', true, /\.md$/)
 
-// 按文件名排序
-nameList = nameList.sort()
+// 存储各个子文件夹下的 Markdown 文件的文件名
+let helloList = []
+let easyList = []
+let mediumList = []
+let hardList = []
 
-// 笔记数量
-let noteNumber = nameList.length - 1
+// 所有 Markdown 文件的文件名
+let allList = []
+
+// 配置 Markdown 组件
+configureComponent(helloComponent, helloList)
+configureComponent(easyComponent, easyList)
+configureComponent(mediumComponent, mediumList)
+configureComponent(hardComponent, hardList)
+
+// 拼接侧边栏需要的文件名列表
+allList = allList.concat(helloList, easyList, mediumList, hardList)
+
+// 文件名排序
+allList = allList.sort()
+
+// 统计不同难度题目的笔记数量
+let easyNumber = easyList.length
+let mediumNumber = mediumList.length
+let hardNumber = hardList.length
+
+// 所有笔记的数量
+let allNumber = easyNumber + mediumNumber + hardNumber
 
 // 默认显示欢迎页
 let hello = '0000.Hello'
@@ -66,6 +109,46 @@ let markdownName = ref(hello)
 // 切换 Markdown 文件
 const chooseMarkdown = (name) => {
     markdownName.value = name
+}
+
+const clickTag = (e) => {
+    // 获取 el-tag 标签上的文本
+    let text = e.currentTarget.children[0].outerText
+    // 通知上文本的颜色
+    let color = ''
+    // 通知类型
+    let type = ''
+
+    // 判断点击标签的文本开头
+    switch (text.split(' ')[0]) {
+        case 'Easy':
+            color = '#00AF9B'
+            type = 'success'
+            break
+        case 'Medium':
+            color = '#FFB822'
+            type = 'warning'
+            break
+        case 'Hard':
+            color = '#FF2D55'
+            type = 'error'
+            break
+        case 'All':
+            color = '#AF52DE'
+            type = 'info'
+            break
+        default:
+            break
+    }
+
+    // 弹出 el-notification 通知
+    ElNotification({
+        dangerouslyUseHTMLString: true,
+        message: '<span style="color: ' + color + ';">' + text + '</span>',
+        type: type,
+        showClose: false,
+        duration: 5000
+    })
 }
 </script>
 
