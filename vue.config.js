04b17a1587a5babefbe1a701121b89088847f082
zhouboyi <1144188685@qq.com>
2022-09-01 14:29:38 +0800

    使用SCSS编写样式

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
+    // 开发配置
     devServer: {
         host: '127.0.0.1',
         port: 18003,
         open: true
     },
+    // Webpack 配置
     chainWebpack: config => {
         config.module.rule('md')
             .test(/\.md/)
@@ -15,5 +17,15 @@ module.exports = defineConfig({
             .use('vue-markdown-loader')
             .loader('vue-markdown-loader/lib/markdown-compiler')
             .options({ raw: true })
+    },
+    // CSS 配置
+    css: {
+        loaderOptions: {
+            // SASS 配置
+            sass: {
+                // 全局引入样式文件
+                additionalData: `@import "@/assets/style/variable.scss";`
+            }
+        }
     }
 })
