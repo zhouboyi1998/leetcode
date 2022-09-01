bfe497aceef246433bd443c6ccaf0b6009994beb
zhouboyi <1144188685@qq.com>
2022-09-01 14:29:38 +0800

    feat(scss): 使用SCSS编写样式

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
     // 禁用对依赖包的转译处理
     transpileDependencies: false,
-    // 加载 Markdown 格式的文件
+    // Webpack 配置
     chainWebpack: config => {
-        config.module.rule('md')
+        // 加载 Markdown 格式的文件
+        config.module
+            .rule('md')
             .test(/\.md/)
             .use('vue-loader')
             .loader('vue-loader')
@@ -14,6 +17,16 @@ module.exports = defineConfig({
             .options({
                 raw: true
             })
+    },
+    // CSS 配置
+    css: {
+        loaderOptions: {
+            // SASS 配置
+            sass: {
+                // 全局引入样式文件
+                additionalData: `@import "@/assets/style/variable.scss";`
+            }
+        }
     },
     // 开发配置
     devServer: {
