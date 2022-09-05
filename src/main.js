5a2be7733e3f480f9c1bf4bd580b4dc743462724
zhouboyi <1144188685@qq.com>
2022-09-05 09:41:27 +0800

    chore(state): 使用Pinia替换Vuex，保存当前选中菜单项的相关信息

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persist'
 import '@/assets/style/index.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'github-markdown-css'
 import 'highlight.js/styles/github.css'
 
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+// 新建 Pinia Store
+const pinia = createPinia()
+
+// Pinia 中添加固化插件
+pinia.use(piniaPersist)
+
+// 添加 Vue-Router 插件 / Pinia 插件 / Element-Plus 插件
+createApp(App).use(router).use(pinia).use(ElementPlus).mount('#app')
