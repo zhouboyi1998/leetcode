8a3a52db4e7b927c42481806c026e4cbbffa3f0a
zhouboyi <1144188685@qq.com>
2022-09-05 09:41:27 +0800

    chore(state): 使用Pinia替换Vuex，保存当前选中菜单项的相关信息

---
+++ src/store/markdown.js
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia'
+import variable from '@/assets/style/variable.module.scss'
+
+// 获取 SCSS 变量
+const helloColor = variable.helloColor
+
+export const useMarkdownStore = defineStore('MARKDOWN', {
+    state: () => ({
+        name: '0000.Hello',
+        color: helloColor
+    }),
+    getters: {},
+    actions: {},
+    persist: {
+        enabled: true,
+        strategies: [
+            {
+                storage: sessionStorage
+            }
+        ]
+    }
+})
